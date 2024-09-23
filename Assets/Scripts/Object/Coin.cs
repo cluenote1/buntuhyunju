4dfd6a246fe750681666f4f88c7c7a35a2db4e8f
cluenote1 <hyeonjuny68@gmail.com>
2024-09-23 22:10:17 +0900

    현피 워

---
+++ Assets/Scripts/Object/Coin.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private GameObject coinEffect;
+
+    private void Start()
+    {
+        // 필요할 경우 물체가 좀 더 잘 보이도록 위치를 조정
+        transform.localPosition += Vector3.back * 0.1f;
+    }
+
+    private void MakeEffect()
+    {
+        // 파티클 효과 생성
+        GameObject effect = Instantiate(coinEffect, transform.position, Quaternion.identity);
+        effect.transform.parent = transform.parent; // 부모 설정
+        effect.GetComponent<ParticleSystem>().Play();
+    }
+
+    // 2D 충돌 처리를 위해 OnTriggerEnter2D로 수정
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 충돌 감지 로그 추가
+        Debug.Log("충돌 감지: " + other.gameObject.name);
+
+        // 플레이어가 코인에 충돌했는지 확인
+        if (other.CompareTag("Player"))
+        {
+            // 점수 증가
+            if (CompareTag("Silver"))
+            {
+                DataManager.Instance.Score += 5;
+            }
+            else if (CompareTag("Gold"))
+            {
+                DataManager.Instance.Score += 50 - (5 * DataManager.Instance.Stage);
+            }
+
+            // 소리 재생 및 효과 생성
+            SoundManager.Instance.SFXPlay(transform.tag, clip);
+            MakeEffect();
+
+            // 코인 비활성화
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Object/WireController.cs
+++ Assets/Scripts/Object/WireController.cs
@@ -10,6 +10,7 @@ public class WireController : MonoBehaviour
     [SerializeField] private float maxDistance = 9f;
     [SerializeField] private AudioClip shotSound;
     [SerializeField] private LayerMask wireBuilding;
+    [SerializeField] private SpriteRenderer wireSprite; // 와이어 스프라이트 렌더러
 
     private float wireDistance;
     public float WireDistance => wireDistance;
@@ -17,14 +18,8 @@ public class WireController : MonoBehaviour
     private bool onWire;
     public bool OnWire => onWire;
 
-    private LineRenderer line;
     private RaycastHit2D hit; // 2D RaycastHit
     private bool isWire;
-
-    private void Awake()
-    {
-        line = GetComponent<LineRenderer>();
-    }
 
     private void Start()
     {
@@ -33,8 +28,17 @@ public class WireController : MonoBehaviour
 
     private void Update()
     {
-        line.SetPosition(0, playerHand.position);
-        line.SetPosition(1, transform.position);
+        // 와이어 스프라이트의 위치를 플레이어의 손 위치와 동기화
+        if (isWire)
+        {
+            wireSprite.transform.position = (playerHand.position + transform.position) / 2;
+            wireSprite.size = new Vector2(Vector3.Distance(playerHand.position, transform.position), wireSprite.size.y);
+
+            // 회전 설정
+            Vector3 direction = (transform.position - playerHand.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            wireSprite.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 
     public void ShotWire()
@@ -42,7 +46,7 @@ public class WireController : MonoBehaviour
         if (isWire) return;
         SoundManager.Instance.SFXPlay("ShotWire", shotSound);
         isWire = true;
-        line.enabled = true;
+        wireSprite.enabled = true;  // 와이어 스프라이트 활성화
         StartCoroutine("Shot");
     }
 
@@ -50,7 +54,7 @@ public class WireController : MonoBehaviour
     {
         onWire = false;
         isWire = false;
-        line.enabled = false;
+        wireSprite.enabled = false; // 와이어 스프라이트 비활성화
         transform.parent = null;
     }
 
@@ -66,6 +70,9 @@ public class WireController : MonoBehaviour
             wireDistance = Vector3.Distance(transform.position, targetPos + lastHandPos);
             transform.position = Vector3.MoveTowards(transform.position, targetPos + lastHandPos, Time.deltaTime * shotSpeed);
 
+            // 스프라이트 크기 조정 (와이어가 길어지거나 줄어드는 효과)
+            wireSprite.size = new Vector2(wireDistance, wireSprite.size.y);
+
             // 2D Raycast 사용
             hit = Physics2D.Raycast(lastHandPos, (targetPos - lastHandPos).normalized, maxDistance - wireDistance, wireBuilding);
             if (hit.collider != null)
@@ -75,6 +82,7 @@ public class WireController : MonoBehaviour
             }
             yield return null;
         }
+
         if (wireDistance == 0f)
             DisableWire();
     }
